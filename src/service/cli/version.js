b43d190efc72990111acb25d7233acc6bf9d9fd2
Igor Antonov <antonov.igor.khv@gmail.com>
2020-01-23 18:03:21 +1000

    Раздел 2

--- src/service/cli/version.js
+++ src/service/cli/version.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const chalk = require(`chalk`);
+
 const packageJsonFile = require(`../../../package.json`);
 
 module.exports = {
   name: `--version`,
   run() {
     const version = packageJsonFile.version;
-    console.info(version);
+    console.info(chalk.blue(version));
   }
 };
