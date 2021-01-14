7b09858397a17eab764e756a97cc29326a6bf628
Igor Antonov <antonov.igor.khv@gmail.com>
2021-01-14 17:51:31 +0300

    Раздел 5

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -4,12 +4,14 @@ const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 const server = require(`./server`);
+const fill = require(`./fill`);
 
 const Cli = {
   [generate.name]: generate,
   [help.name]: help,
   [version.name]: version,
   [server.name]: server,
+  [fill.name]: fill,
 };
 
 module.exports = {
